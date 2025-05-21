987e103709820b87d486a8b74983a8162b5efda2
DjordjeSajic <djordje.sajic5@gmail.com>
2025-05-21 19:21:31 +0200

    added fetch for movie details

--- src/api/fetchMovies.js
+++ src/api/fetchMovies.js
@@ -4,17 +4,35 @@ export const fetchMovies= async(searchText,moviesCallback,errorCallback) =>{
         const data=await response.json();
     
 
-        if(data.Response=='True'){
-        moviesCallback(data.Search)
-        errorCallback(null);
+        if(data.Response==='True'){
+            const movieDetailsPromis=data.Search.map((movie) => fetchMovieDetails(movie.imdbID, errorCallback))
+            const movieDetails= await Promise.all(movieDetailsPromis);
+
+            moviesCallback(movieDetails)
+            errorCallback(null);
         }else{
-        moviesCallback([]);
-        errorCallback(data.Error);
+            moviesCallback([]);
+            errorCallback(data.Error);
         }
 
 
     }catch(err){
         moviesCallback([]);
         errorCallback('An error occured while fetching movie data');
+    }
+};
+
+const fetchMovieDetails=async (id,errorCallback) => {
+    try{
+        const response= await fetch(`http://www.omdbapi.com/?i=${id}&plot=full&apikey=f762b390`)
+        const data=await response.json();
+
+        if(data.Response === 'True'){
+            return data;
+        }else{
+            throw new Error(data.Error);
+        }
+    }catch(err){
+        errorCallback('An error occurred while fetching movie details');
     }
 };
