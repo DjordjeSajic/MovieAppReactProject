10d430834c826954eadb95b313045f099b459ebd
DjordjeSajic <djordje.sajic5@gmail.com>
2025-05-21 17:24:43 +0200

    placeholder for search bar

--- src/components/MoivesPortal.js
+++ src/components/MoivesPortal.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 function MoviePortal(){
+
+    const [searchInputText,setSearchInputText]=useState('')
+    const [enteredSearchText,setEnteredSearchText]=useState('')
+    
+    const onEnteredSearch= (e) =>{
+        e.preventDefault();
+        setEnteredSearchText(searchInputText);
+    };
+    
     return(
-        <div>
-            MoviePortal
+        <>
+        <div className="row">
+            <div className="col-md-12">
+            <form onSubmit={onEnteredSearch}>
+                <input
+                    type="text" placeholder="Search" className="form-control"
+                    value={searchInputText}
+                    onChange={(e) => setSearchInputText(e.target.value)} 
+                    />
+            </form>
+            </div>
         </div>
+        {enteredSearchText}
+        </>
     );
 }
 export default MoviePortal;
