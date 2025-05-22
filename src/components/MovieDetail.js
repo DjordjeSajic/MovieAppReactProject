75239974cee6399a43d2de96428a396d1e13a870
DjordjeSajic <djordje.sajic5@gmail.com>
2025-05-22 13:02:11 +0200

    added css for movie img

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,9 +1,13 @@
+import '../css/MovieDetail.css'
 function MovieDetail({movie}){
    return (
         <>
             <div className="row">
-                <div className="col-md-2">
-                    <img src={movie.Poster} alt={movie.Title} width='100%' />
+                <div className="col-md-2 image-container">
+                    <img src={movie.Poster} alt={movie.Title} width='100%'height='80%' />
+                    <div className='overlay d-flex align-items-center justify-content-center'>
+                          <a href={`https://www.youtube.com/results?search_query=${movie.Title} trailer`}>Watch Trailer</a>
+                    </div>
                 </div>
                 <div class='col-md-10'>
                     <div class="card">
@@ -12,7 +16,6 @@ function MovieDetail({movie}){
                             <h6 class="card-subtitle mb-2 text-muted">{movie.Released}</h6>
                             <p class="card-text">{movie.Plot}</p>
                             <a href={`https://www.imdb.com/title/${movie.imdbID}`} class="card-link">IMDB </a>
-                            <a href={`https://www.youtube.com/results?search_query=${movie.Title} trailer`}>Watch Trailer</a>
                         </div>
                     </div>
                 </div>
